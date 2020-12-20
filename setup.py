c5e699d721f14856314d8cb9235c2b2fada302af
schperplata <domen.jurkovic@gmail.com>
2020-12-20 23:11:23 +0100

    Updated links and other info

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 import setuptools
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="dlpt",
     version="1.0.0",
     author="Damogran Labs",
     author_email="info@damogranlabs.com",
-    description="Damogran Labs Common Python Tools",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    description="Damogran Labs Python Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/damogranlabs/dlpt",
+    packages=["dlpt"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
+    install_requires=["jsonpickle", "psutil"]
 )
