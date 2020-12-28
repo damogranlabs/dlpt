007856cecf04807118c0e56b28bb304d7952da23
schperplata <domen.jurkovic@gmail.com>
2020-12-28 23:23:07 +0100

    Minor fixes and package environment upgrade

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dlpt",
-    version="1.0.0",
+    version="1.0.1",
     author="Damogran Labs",
     author_email="info@damogranlabs.com",
     description="Damogran Labs Python Tools",
