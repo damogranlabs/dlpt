b8d929c218964288cc833665bdb96accee5e190e
schperplata <domen.jurkovic@gmail.com>
2021-01-17 22:43:35 +0100

    github test 3

--- tests/test_github.py
+++ tests/test_github.py
@@ -16,5 +16,14 @@ def test_create_file(tmp_path):
         writer.write(CONTENT)
     assert os.path.exists(p2)
 
+    p3 = os.path.join(d, "dir1", "dir1")
+    os.makedirs(p3)
+    assert os.path.exists(p3)
+
+    p3_2 = os.path.join(p3, "hello3.txt")
+    with open(p3_2, "w+") as writer:
+        writer.write(CONTENT)
+    assert os.path.exists(p3_2)
+
     assert p.read_text() == CONTENT
     assert len(list(tmp_path.iterdir())) == 1
