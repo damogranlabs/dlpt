687ad0075faf9fc44e75ad06b0270a147ba662ff
schperplata <domen.jurkovic@gmail.com>
2022-02-13 20:04:59 +0100

    Added all used pythons for a local development

--- createVenv.bat
+++ createVenv.bat
@@ -1,10 +1,11 @@
 @echo off
 
-SET PY_EXE_PATH="C:\Python3.8.7\python.exe"
+SET PY_EXE_PATH="C:\Python36\python.exe"
+REM SET PY_EXE_PATH="C:\Python3.8.7\python.exe"
+REM SET PY_EXE_PATH="C:\Python310\python.exe"
 
 %PY_EXE_PATH% -m venv ./env
 CALL .\env\Scripts\activate.bat
-
 
 python -m pip install -U pip
 pip install -r requirements.txt
