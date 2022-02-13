58070bb42b7b339de9ae121cba7033f0aa4bfef8
schperplata <domen.jurkovic@gmail.com>
2022-02-13 20:25:26 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dlpt",
-    version="2.1.0",
+    version="3.0.0",
     author="Damogran Labs",
     author_email="info@damogranlabs.com",
     description="Damogran Labs Python Tools",
